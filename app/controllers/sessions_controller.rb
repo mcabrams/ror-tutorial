d195e9861ff0ad4069f2f77a4c0660e0ceade5d2
Matthew Abrams <mcabrams1@gmail.com>
2014-09-17 22:23:07 -0500

    switch signin form to using form_tag

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,9 +3,9 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by_email(params[:session][:email].downcase)
+    user = User.find_by_email(params[:email].downcase)
 
-    if user && user.authenticate(params[:session][:password])
+    if user && user.authenticate(params[:password])
       sign_in user
       redirect_to user
       # sign user in and redirect to the user's show page
