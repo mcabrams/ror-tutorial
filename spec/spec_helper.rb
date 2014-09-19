0f4a6b5fe56fc5f5e0b48af575dc199c7bdbccb7
Matthew Abrams <mcabrams1@gmail.com>
2014-09-19 18:16:07 -0500

    add some helper functions for tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -42,5 +42,5 @@ end
 
 Spork.each_run do
   # This code will be run each time you run your specs.
-
+  Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f} # Doesn't seem to be working - MCA
 end
